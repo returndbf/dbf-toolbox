78a8dbc12783dcd625599dbd29f21b8e8c587d2b
returndbf <1668959325@qq.com>
2024-12-08 22:23:39 +0800

    fix: 使用原来的动画😂

--- entrypoints/popup/weather/components/Accordion.tsx
+++ entrypoints/popup/weather/components/Accordion.tsx
@@ -8,9 +8,9 @@ interface Props {
 }
 
 const Accordion = ({title = '', children}: Props) => {
-    const [isActive, setActive] = useState(false);
+    const [isActive, setActive] = useState(true);
     return (
-        <div>
+        <div style={{cursor:"pointer"}} >
             <div className={'head'} onClick={() => setActive(!isActive)}>
                 <div className={[isActive ? 'activate-panel' : 'deactivate-panel'].join(' ')}>
                     <img className={'panel-arrow-img'} src={back}/>
@@ -18,9 +18,9 @@ const Accordion = ({title = '', children}: Props) => {
                 <div className={'head-title'}>{title}</div>
             </div>
 
-            <div className={[isActive?'content':'empty'].join(' ')}>
+            {<div className={[isActive ? 'content' : 'empty'].join(' ')}>
                 {children}
-            </div>
+            </div>}
         </div>
     );
 };
